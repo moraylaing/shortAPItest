ccbb94e6b05b32698406a901efb0607aabf01a6d
Glitch (morayshortened-api) <none>
2020-04-26 23:07:39 +0000

    🖇👗 Updated with Glitch

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const app = express();
+const app = express(); 
+let testshortURL='';
+let testredirectURL='';
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
@@ -49,13 +51,22 @@ app.listen(port, function () {
   console.log('Node.js listening ...');
 });
 
-app.get("/api/shorturl/new/:url(*)", function (req, res, next) {
- // console.log("GET");  
+app.get("/api/shorturl/new/:url(*)", function (req, res) {
+  console.log("GET The URL");  
   var {url}= req.params; 
+  let htmlString="<!DOCTYPE html><html> <head>";
+  htmlString+=" <script src='app.js'></script>";
+  htmlString+="    <title>Morays URL Shortener</title>";
+  htmlString+="     <link rel='shortcut icon' href='https://cdn.hyperdev.com/us-east-1%3A52a203ff-088b-420f-81be-45bf559d01b1%2Ffavicon.ico' type='image/x-icon'/> ";
+  htmlString+="     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>";
+  htmlString+="     <link href='/public/style.css' rel='stylesheet' type='text/css'>";
+  htmlString+="  </head>";
+  htmlString+="  <body";
+  htmlString+="     <div class='container'>";
   
-  var regex  =/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
-  
-  
+  if(url!="app.js")
+  {
+      var regex  =/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
   if(regex.test(url)===true)
     {
       var shortNum = Math.floor(Math.random()*100000).toString();
@@ -64,19 +75,36 @@ app.get("/api/shorturl/new/:url(*)", function (req, res, next) {
         orig_url: url,
         short_url: shortNum
       });
-      data.save(err=>{
-        if(err){
-          return res.send('Error saving to database');
+      var test=true;
+      data.save(err=>
+      { 
+        
+        if(err)
+        {
+           res.sendFile(process.cwd() + '/views/error.html');
         }  
-      });
-      return res.json(data);
+      }); 
+        testshortURL = shortNum;
+        testredirectURL=data.orig_url;
+        //console.log(testshortURL+"-"+testredirectURL);
+      htmlString+=" <h2>Lets test that short URL</h2>";
+  htmlString+=" <section>";
+  htmlString+="   <a href='/api/testurl'>https://morayshortened-api.glitch.me/api/shorturl/redirect/"+shortNum+" </a>";
+  htmlString+=" </section>";
+  htmlString+="  </body>";
+  htmlString+=" </html>";
+        return res.send(htmlString);
+        //return res.sendFile(process.cwd() + '/views/redirect.html');
+    
+      //return res.json(data); 
     }
-    var data = new shorturl(
-      {
-        orig_url: "URL DOES MEET REQUIREMENTS FOR A VALID URL",
-        short_url: shortNum
-      });
-    return res.json(data);
+    //var data = new shorturl(
+      //{
+      //  orig_url: "URL DOES MEET REQUIREMENTS FOR A VALID URL",
+      //  short_url: shortNum
+     // });
+    return res.sendFile(process.cwd() + '/views/invalid.html');
+    //return res.json(data);
   
   
   //return res.json({url});
@@ -85,11 +113,41 @@ app.get("/api/shorturl/new/:url(*)", function (req, res, next) {
   //(Step 7) 
   //(Step 8)
   //(Step 9)
+  }
+  else
+  {
+    return res.sendFile(process.cwd() + '/views/redirect.html');
+
+  }
+  
+  
 });
+app.get("/api/testurl",(req,res)=>
+{
+  //console.log("test URL  "+testshortURL); 
+  var strToCheck = testredirectURL;  
+  
+     // console.log(strToCheck);
+
+  var re = new RegExp("^(http|https)://","i");
+    if(re.test(strToCheck)) 
+    { 
+      res.redirect(301,strToCheck);
+    }
+    else
+    {
+      console.log("redirect");
+      res.redirect(301,'http://'+strToCheck);
+    }
+   
+
+});
+
+
 app.get("/api/shorturl/redirect/:urlToForward(*)",(req,res,next)=>{
   //console.log("Redirect"); 
   var {urlToForward}= req.params; 
-  //console.log(urlToForward);
+  //console.log(urlToForward); 
 
   shorturl.findOne({'short_url':urlToForward}, (err,data)=>{
     if(err) return res.send('Error reading database');
@@ -97,7 +155,8 @@ app.get("/api/shorturl/redirect/:urlToForward(*)",(req,res,next)=>{
     var re = new RegExp("^(http|https)://","i");
     var strToCheck = data.orig_url;
     console.log(strToCheck);
-    if(re.test(strToCheck)){ 
+    if(re.test(strToCheck))
+    { 
       res.redirect(301,data.orig_url);
     }
     else
